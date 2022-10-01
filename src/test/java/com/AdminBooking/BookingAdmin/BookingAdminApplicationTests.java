be5d91873c91eac4e2c434948586a8fb34752b6b
Bhavya Kalra <bkalra08032002@gmail.com>
2022-10-01 18:39:41 +0530

    initial commit

---
+++ src/test/java/com/AdminBooking/BookingAdmin/BookingAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdminBooking.BookingAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
